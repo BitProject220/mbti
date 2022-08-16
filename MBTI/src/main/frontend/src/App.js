fa0ed4c2d49e1c51bf69a5e5566acad9a896ab71
YuJaenam <100551334+YuJaenam@users.noreply.github.com>
2022-08-16 13:09:42 +0900

    test detail 추가

--- MBTI/src/main/frontend/src/App.js
+++ MBTI/src/main/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Main from './main/Main';
 import Test from './test/Test';
+import SignupMain from './sign/SignupMain';
 import './css/reset.css';
 import MBTIDetail from './DetailApp/Detailpage/MBTIDetail';
 
@@ -18,27 +19,14 @@ const App = () => {
       </nav> 
       
       <Routes>
-        {/* 상연 */}
-
-        {/* 재남 */}
-
-        {/* 유진 */}
-
-        {/* 소윤 */}
-
-        {/* 기진 */}
-
-        {/* 범주 */}
-
-        {/* 승찬 */}
+        <Route path="/signUp" element={<SignupMain /> } />
         <Route path="/" element={<Main/>} />
         <Route index element={<Main/>} />         
         <Route path="/test" element={<Test/>} />
-        <Route path="/Main" />
-        <Route index element={<Main/>} />
         <Route path="/Main">
           <Route index element={<Main/>} />
         </Route>
+
         <Route path="/MBTIDetail" element={<MBTIDetail /> } />
       </Routes>
       </>
---
+++ MBTI/src/main/frontend/src/test/TestResult.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './testResult.css';
+const TestResult = () => {
+  return (
+    <>
+      <section>
+        <div className='resultMain'>
+          <div>
+            <h1>당신의 성격 유형은</h1>
+          </div>
+          <div>
+            '성격유형자리'
+          </div>
+          <div>
+            'MBTI'
+          </div>
+          <div>
+            사진
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TestResult;
