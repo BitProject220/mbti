594730e7dba8e8058a15f8bc0bc05f0dd7f6eb60
soyoon <soyoon0629@gmail.com>
2022-08-18 13:32:07 +0900

    48-mainpage

--- MBTI/src/main/frontend/src/App.js
+++ MBTI/src/main/frontend/src/App.js
@@ -19,20 +19,14 @@ import ContactMain from './Contact/ContactMain';
 import LoginPage from './Login/LoginPage';
 import Signup from './sign/Signup';
 
-
 const App = () => {
   return (
     <BrowserRouter>
       <>
-      <nav>
-        <ul>
-          <li><Link to="/">Main</Link></li>
-          <li><Link to="/signUp">Signup</Link></li>
-        </ul>
-      </nav> 
       
       <Routes>
 
+      <Route path="/" element={<Main/>} />
 
       <Route path="/Mbtiintrodusion" element={<Mbtiintrodusion id="1" /> } />
       <Route path="/MbtiSW" element={<MbtiSW id="1" /> } />
@@ -53,20 +47,17 @@ const App = () => {
         <Route path="/kakaoLogin" element={<KakaoRedirectHandler/>}/> 
 
         <Route path="/signUp" element={<SignupMain /> } />
-        <Route path="/" element={<Main/>} />
         <Route path="/ContactMain" element={<ContactMain/>}/>
         <Route path="/LoginPage" element={<LoginPage />}/>
-        <Route path="/kakaoLogin" element={<KakaoRedirectHandler />}/> 
-        <Route index element={<Main/>} />         
+        <Route path="/kakaoLogin" element={<KakaoRedirectHandler />}/>         
         <Route path="/test" element={<Test/>} />
-        <Route path="/Main">
-          <Route index element={<Main/>} />
-        </Route>
+
 
       </Routes>
       </>
     </BrowserRouter>
 
+    
   )
   
 };
