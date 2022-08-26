41bdf4976539f1ebc56ff9cab542b02dadf1eacf
yujinParking <dbwlsdhksenz@naver.com>
2022-08-26 18:31:32 +0900

    132-회원가입유효성완료

--- MBTI/src/main/java/com/mbti/MBTI/user/controller/UserController.java
+++ MBTI/src/main/java/com/mbti/MBTI/user/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.google.gson.Gson;
 import com.mbti.MBTI.user.bean.UserDTO;
 import com.mbti.MBTI.user.service.UserService;
 
@@ -38,7 +38,6 @@ public class UserController {
 	
 	@PostMapping(value="/user/write")
 	public void write(@RequestBody UserDTO userDTO) {
-		
 		System.out.println(userDTO);
 		userService.write(userDTO);
 	}
@@ -46,13 +45,15 @@ public class UserController {
 	//###################유진 시작#################################
 	
 	@PostMapping(value = "/user/emailCheck")
-	public String emailCheck(@RequestBody String email) {
-		System.out.println(email.toString());
-		return userService.emailCheck(email.toString());
+	public String emailCheck(@RequestParam String email) {
+		  
+		System.out.println("이메일은-------------------------------"+email);
+		return userService.emailCheck(email);
+		
 	}
 	
-	@GetMapping(value = "/user/emailNumCheck")
-	public String emailNumCheck(String email) throws Exception{
+	@PostMapping(value = "/user/emailNumCheck")
+	public String emailNumCheck(@RequestParam String email) throws Exception{
 			logger.info("이메일 인증 요청이 들어옴!"+email);
 	        logger.info("인증번호 : " + email);
 			//return  mailService.joinEmail(user_email);
@@ -92,7 +93,7 @@ public class UserController {
 	}
 	
 	@PostMapping(value = "/user/nameCheck")
-	public String nameCheck(@RequestBody String name) {
+	public String nameCheck(@RequestParam String name) {
 		System.out.println("이름은"+name);
 		return userService.nameCheck(name);
 	}
