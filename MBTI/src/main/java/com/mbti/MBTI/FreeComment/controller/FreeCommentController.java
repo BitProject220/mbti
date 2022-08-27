3871359f234352417d4526dc915f614a4b92f6f6
jo <luckghost123@naver.com>
2022-08-27 17:47:48 +0900

    146_FreeComment_springboot_bumjoo

---
+++ MBTI/src/main/java/com/mbti/MBTI/FreeComment/controller/FreeCommentController.java
@@ -0,0 +1,32 @@
+package com.mbti.MBTI.FreeComment.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mbti.MBTI.FreeComment.bean.FreeCommentDTO;
+import com.mbti.MBTI.FreeComment.service.FreeCommentService;
+import com.mbti.MBTI.user.service.UserService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class FreeCommentController {
+	
+	@Autowired
+	private FreeCommentService freeCommentService;
+	
+	//###################범주 시작#################################
+	
+	@GetMapping(value = "/reply/userComment")
+	@ResponseBody
+	public List<FreeCommentDTO> userComment() {
+		return freeCommentService.userComment();
+	}
+	
+	//###################범주 끝#################################
+}
